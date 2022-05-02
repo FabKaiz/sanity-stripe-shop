ded4ffc149027d74a71b7e343796863b52cfbbb1
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-02 11:47:40 +0200

    banner setup

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,27 +1,39 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components'
+import { client } from '../lib/client'
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Our lastest tech products</p>
       </div>
 
-      <Product />
-
       <div className="products-container">
-        {['Product 1', 'Product 2'].map((product) => {
-          return product
+        {products?.map((product) => {
+          return product.name
         })}
       </div>
       
       <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  }
 }
 
 export default Home
