57c010c5abe961d150f054ecfc99eaae15f0104e
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-02 12:43:00 +0200

    added Product and FooterBanner component

--- app/pages/index.js
+++ app/pages/index.js
@@ -6,7 +6,6 @@ const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      {console.log(bannerData)}
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
@@ -14,22 +13,22 @@ const Home = ({ products, bannerData }) => {
       </div>
 
       <div className="products-container">
-        {products?.map((product) => {
-          return product.name
-        })}
+        {products?.map((product) => (
+          <Product product={product} key={product._id} />
+        ))}
       </div>
-      
-      <FooterBanner />
+
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   )
 }
 
 export const getServerSideProps = async () => {
-  const query = '*[_type == "product"]';
-  const products = await client.fetch(query);
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
 
-  const bannerQuery = '*[_type == "banner"]';
-  const bannerData = await client.fetch(bannerQuery);
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
 
   return {
     props: { products, bannerData },
