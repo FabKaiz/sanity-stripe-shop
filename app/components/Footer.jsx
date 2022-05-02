13440154d09fd1cbbc52be275a2a56fa1ce07cd5
Fabien Chareun <cacrfabfr@gmail.com>
2022-05-02 14:10:00 +0200

    footer done

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>
+        2022 FabKaiz Shop - This is a fake website to train web development
+        skills
+      </p>
+      <p className="icons">
+        <a href="https://github.com/FabKaiz" target="_blank">
+          <AiOutlineGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/fabien-chareun/" target="_blank">
+          <AiOutlineLinkedin />
+        </a>
+      </p>
+    </div>
   )
 }
 
